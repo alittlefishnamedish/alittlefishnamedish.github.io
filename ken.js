f6b5811f44e435dbb26b4084bbd3022ad60d7561
alittlefishnamedish <129694411+alittlefishnamedish@users.noreply.github.com>
2024-07-08 23:23:07 -0700

    Create ken.js

---
+++ ken.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+    simple()
+}
+function simple(){
+    var a = document.getElementById("ideas")
+    a.innerHTML = " "
+    var something =""
+    Object.keys(localStorage).forEach(function(key,index){
+      var c = localStorage.getItem(key)
+      something+=(index+1)+": "+c+"<br>"
+}
+)
+a.innerHTML = something
+
+}
+function welcome(){
+    var e = document.getElementById("fan").value
+    var f = localStorage.length
+    localStorage.setItem(f,e)
+
+
+simple()
+}
